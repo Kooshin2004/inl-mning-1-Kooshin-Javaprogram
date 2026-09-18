fde434d8ea756088d83085f72e8e528e64ab193c
Kooshin2004 <kaynanshe@gmail.com>
2026-09-18 23:16:01 +0200

    Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,23 @@
+public class Player {
+    private String firstName;
+    private String lastName;
+    private int score;
 
+    public Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void addToScore(int score) {
+        this.score = this.score + score;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
